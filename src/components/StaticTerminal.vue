d40b98dc0e9be3a9b241e80e01f93956d8d25da8
Carsten König <carstenkoenig92@gmail.com>
2018-02-01 13:34:06 +0100

    initial

---
+++ src/components/StaticTerminal.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Test</div>
+</template>
+
+<script>
+  export default {
+    data() {
+        return {
+            test: 5
+        }
+    }
+  }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: path.resolve(__dirname + '/src/components/StaticTerminal.vue'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'vue-static-terminal.min.js',
+    library: 'vue-static-terminal',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin( {
+      minimize : true,
+      sourceMap : false,
+      mangle: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
