e73a3ab48a7bc6f6a12c7e43db969c83ee89f953
Carsten König <carstenkoenig92@gmail.com>
2018-02-08 17:57:10 +0100

    adds some basic css

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <div>
-    <h1>Static terminal test</h1>
-    <static-terminal></static-terminal>
-  </div>
+    <div>
+        <h1>Static terminal test</h1>
+        <div class="grid">
+            <div class="col">
+                <static-terminal></static-terminal>
+            </div>
+            <div class="col">
+                <static-terminal></static-terminal>
+            </div>
+            <div class="col">
+                <static-terminal></static-terminal>
+            </div>
+        </div>
+
+    </div>
 </template>
 
-<script>
-  import StaticTerminal from './components/StaticTerminal.vue'
-
-  export default {
-    components: {
-      StaticTerminal
+<style>
+    * {
+        font-family: sans-serif;
     }
-  }
+
+    .grid {
+        display: flex;
+    }
+
+    .col {
+        flex: 1;
+        margin: 20px;
+    }
+</style>
+
+<script>
+    import StaticTerminal from './components/StaticTerminal.vue'
+
+    export default {
+        components: {
+            StaticTerminal
+        }
+    }
 </script>
